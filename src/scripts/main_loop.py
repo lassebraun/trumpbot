d90bd44682cc59255c60df1e7718c0f83c3e409c
Lasse Braun <lasse.braun03@gmail.com>
2026-04-20 16:29:02 +0200

    bugfix

--- src/scripts/main_loop.py
+++ src/scripts/main_loop.py
@@ -18,7 +18,7 @@ async def main_loop(crud: DatabaseCrud, trade_executor: TradeExecutor) -> None:
         try:
             posts = list(get_latest_posts(since_id=anchor.id if anchor else None))
             new_posts = []
-            
+
             for post in posts:
                 if not crud.get_one(Posts, QueryFactory.by_id(post.id)):
                     crud.save(post)
@@ -30,7 +30,7 @@ async def main_loop(crud: DatabaseCrud, trade_executor: TradeExecutor) -> None:
                 analysis_coros = []
                 for post in new_posts:
                     logger.info(f"Processing post: {post.id}")
-                    if is_text_post(post["text"]):
+                    if is_text_post(post.text):
                         for client in config.Config.analysis_clients:
                             analysis_coros.append(client.analyze_post(post, news_digest))
                     analyses = await asyncio.gather(*analysis_coros)
